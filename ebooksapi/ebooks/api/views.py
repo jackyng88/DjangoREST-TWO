5b0bd4c3e91e7225cfe879b0e65ad19c5325ca5b
Jacky <jacky.ng9188@gmail.com>
2019-05-17 17:14:25 -0400

    Added review feature to serializers/views that extend from a Concrete API View class.

--- ebooksapi/ebooks/api/views.py
+++ ebooksapi/ebooks/api/views.py
@@ -1,9 +1,54 @@
 from rest_framework import generics, mixins
+from rest_framework.generics import get_object_or_404
 
-from ebooks.models import Ebook
-from ebooks.api.serializers import EbookSerializer
+from ebooks.models import Ebook, Review
+from ebooks.api.serializers import EbookSerializer, ReviewSerializer
 
 
+class EbookListCreateAPIView(generics.ListCreateAPIView):
+    '''
+    Class to create an Ebook List API View extending Concrete API View class
+    does the same thing as the commented code at the bottom of this file. 
+    Of course this is at a higher abstraction level. 
+
+    Minor note - In the Browsable API function provided by Django, the
+    HTML Form for various REST HTTP functions is provided because we extended
+    from the GenericAPIView class.
+    '''
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    ''' 
+    Like EbookListCreateAPIView - using a Concrete API View class. Again
+    at a higher abstraction level. Browsing the documentation shows us that
+    this concrete view class extends three different mixins and GenericAPIView.
+    Has functionalities of GET, PUT, PATCH, DELETE.
+    '''
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    # Review Create API View extending from Concrete Create API View class
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        # Overriding the inherited perform_create method 
+        ebook_pk = self.kwargs.get('ebook_pk')
+        ebook = get_object_or_404(Ebook, pk=ebook_pk)
+        serializer.save(ebook=ebook)
+
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    # Review detail API View
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+
+'''
 class EbookListCreateAPIView(mixins.ListModelMixin,
                              mixins.CreateModelMixin,
                              generics.GenericAPIView):
@@ -18,3 +63,4 @@ class EbookListCreateAPIView(mixins.ListModelMixin,
     def post(self, request, *args, **kwargs):
         # Post method
         return self.create(request, *args, **kwargs)
+'''
